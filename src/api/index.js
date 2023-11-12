b2329e8602bb3956eaf0ef05ad7c17d43949b037
Dmytro Liuba <lyubadmitry2@gmail.com>
2023-11-12 11:06:56 +0200

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,15 @@
 const STORAGE_KEY = "tu-do-list";
+
+export function saveData(data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+export function getData() {
+    try {
+        const data = localStorage.getItem(STORAGE_KEY);
+
+        return data ? JSON.parse(data) : [];
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,22 @@
+import { formRef } from "./refs";
+import { saveData, getData } from "./api";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formRef.addEventListener("submit", handleForm);
+
+function handleForm(event) {
+    event.preventDefault();
+
+    const data = Object.fromEntries(new FormData(event.target));
+
+    data.id = Date.now();
+    data.checked = false;
+
+    const dataArr = getData();
+    // const inStorage
+    dataArr.push(data);
+    saveData(dataArr);
+
+    event.target.reset();
+}
